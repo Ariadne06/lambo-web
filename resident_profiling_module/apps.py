219ac336e95faeac097f7f13e66df7f1bf14b76d
Ariadne06 <adrianne.villarta@ctu.edu.ph>
2025-06-25 21:32:42 +0800

    deleted residentprofiling module

--- resident_profiling_module/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ResidentProfilingModuleConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'resident_profiling_module'
