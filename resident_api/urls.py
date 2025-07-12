62b4bba2a14998b94ca44bbc0090ec8e1cc8d24d
Addyabai <addyabai@gmail.com>
2025-07-13 02:35:45 +0800

    initial: resident drf api

--- resident_api/urls.py
+++ resident_api/urls.py
@@ -1,7 +1,18 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import ResidentViewSet
+from .views import (ResidentViewSet, CivilStatusViewSet, EducationalAttainmentViewSet, SitioViewSet, ReligionViewSet, ResidentStatusViewSet, ReligionCategoryViewSet, ResidentRegistrationView)
 
 router = DefaultRouter()
 router.register(r'residents', ResidentViewSet)
+router.register(r'civil-statuses', CivilStatusViewSet)
+router.register(r'educational-attainments', EducationalAttainmentViewSet)
+router.register(r'sitios', SitioViewSet)
+router.register(r'religions', ReligionViewSet)
+router.register(r'resident-statuses', ResidentStatusViewSet)
+router.register(r'religion-categories', ReligionCategoryViewSet)
 
 urlpatterns = router.urls
+
+urlpatterns += [
+    path('register/', ResidentRegistrationView.as_view(), name='resident-register'),
+]
--- resident_api/views.py
+++ resident_api/views.py
@@ -1,10 +1,36 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .serializers import ResidentSerializer
-from resident_profiling_module.models import Resident
-
-# Create your views here.
+from rest_framework import viewsets, generics, status
+from .serializers import ResidentSerializer, SitioSerializer, CivilStatusSerializer, EducationalAttainmentSerializer, ReligionSerializer, ResidentStatusSerializer, ReligionCategorySerializer, ResidentRegistrationSerializer, AddressSerializer
+from resident_profiling_module.models import Resident, Sitio, CivilStatus, EducationalAttainment, Religion, ResidentStatus, ReligionCategory, Address
 
 class ResidentViewSet(viewsets.ModelViewSet):
     queryset = Resident.objects.all()
     serializer_class = ResidentSerializer
+
+class CivilStatusViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = CivilStatus.objects.all()
+    serializer_class = CivilStatusSerializer
+
+class EducationalAttainmentViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = EducationalAttainment.objects.all()
+    serializer_class = EducationalAttainmentSerializer
+
+class SitioViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Sitio.objects.all()
+    serializer_class = SitioSerializer
+
+class ReligionViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Religion.objects.all()
+    serializer_class = ReligionSerializer
+
+class ResidentStatusViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = ResidentStatus.objects.all()
+    serializer_class = ResidentStatusSerializer
+
+class ReligionCategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = ReligionCategory.objects.all()
+    serializer_class = ReligionCategorySerializer
+
+class ResidentRegistrationView(generics.CreateAPIView):
+    queryset = Resident.objects.all()
+    serializer_class = ResidentRegistrationSerializer
+
