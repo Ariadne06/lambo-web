dde9581b9234db2fc20db31842f78b1365b40c31
Addyabai <addyabai@gmail.com>
2025-07-13 02:35:45 +0800

    initial: resident drf api

--- resident_api/serializers.py
+++ resident_api/serializers.py
@@ -1,7 +1,155 @@
 from rest_framework import serializers
-from resident_profiling_module.models import Resident
+from resident_profiling_module.models import Resident, Religion, CivilStatus, EducationalAttainment, Sitio, ResidentStatus, ReligionCategory, Address
+from django.db import connection
+import hashlib
+
+class ReligionSerializer(serializers.ModelSerializer):
+    religion_name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Religion
+        fields = ['religion_id', 'religion_name']
+
+    def get_religion_name(self, obj):
+        # Customize as needed; this is a common pattern:
+        if obj.other_religion:
+            return obj.other_religion
+        return obj.religion_cat.religion_name
+
+
+class SitioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sitio
+        fields = ['sitio_id', 'sitio_name']
+
+class AddressSerializer(serializers.ModelSerializer):
+    sitio = SitioSerializer(read_only=True)
+
+    class Meta:
+        model = Address
+        fields = [
+            'address_id',
+            'house_number',
+            'street',
+            'barangay',
+            'sitio',
+            'city_municipality',
+            'country',
+        ]
+
+
+class CivilStatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CivilStatus
+        fields = ['civil_stat_id', 'civil_name']
+
+class EducationalAttainmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EducationalAttainment
+        fields = ['educational_attain_id', 'educational_attain_name']
+
+
+class ResidentStatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ResidentStatus
+        fields = ['status_id', 'status_name']
+
+class ResidentRegistrationSerializer(serializers.ModelSerializer):
+    religion_cat_id = serializers.IntegerField(write_only=True)
+    # Address fields
+    house_number = serializers.CharField(write_only=True, required=False, allow_blank=True)
+    street = serializers.CharField(write_only=True, required=False, allow_blank=True)
+    barangay = serializers.CharField(write_only=True, required=False, allow_blank=True)
+    sitio_id = serializers.IntegerField(write_only=True, required=False, allow_null=True)
+    city_municipality = serializers.CharField(write_only=True)
+    country = serializers.CharField(write_only=True)
+    
+    # Nested serializers for response
+    religion = ReligionSerializer(read_only=True)
+    address = AddressSerializer(read_only=True)
+    civil_status = CivilStatusSerializer(read_only=True)
+    educational_attainment = EducationalAttainmentSerializer(read_only=True)
+    status = ResidentStatusSerializer(read_only=True)
+
+    class Meta:
+        model = Resident
+        fields = [
+            'last_name', 'first_name', 'middle_name', 'suffix', 'dob', 'sex', 'gender',
+            'is_voter', 'email', 'phone_number', 'date_recorded',
+            'civil_status', 'educational_attainment', 'status', 'address', 'religion',
+            'religion_cat_id', 'house_number', 'street', 'barangay', 'sitio_id', 'city_municipality', 'country',
+        ]
+
+    def create(self, validated_data):
+        
+        data = self.context['request'].data
+         
+        plain_password = data.get('password', '')
+        password_hashed = hashlib.sha256(plain_password.encode()).hexdigest()
+        
+        params = [
+            data.get('last_name'),
+            data.get('first_name'),
+            data.get('dob'),
+            data.get('sex'),
+            data.get('barangay'),
+            data.get('city_municipality'),
+            data.get('status'),  
+            data.get('username'),
+            password_hashed,  
+            data.get('doc_type', 'ID'),  
+            data.get('image_base64', ''),  
+            data.get('middle_name', ''),  
+            data.get('suffix', ''),  
+            data.get('gender', ''),  
+            data.get('is_voter', False),
+            data.get('email', ''), 
+            data.get('phone_number', ''),  
+            data.get('religion_cat_id'),
+            data.get('other_religion', ''), 
+            data.get('civil_status'),  
+            data.get('educational_attainment'), 
+            data.get('house_number', ''),  
+            data.get('street', ''),  
+            data.get('sitio_id'),
+            data.get('country', 'Philippines'),
+            data.get('req_pass_change', False),
+            data.get('document_type', ''),  
+            data.get('document_number', ''),  
+            data.get('uploaded_by', 1),  
+        ]
+
+    
+        with connection.cursor() as cursor:
+            cursor.execute("""
+                SELECT register_verified_resident(
+                    %s, %s, %s, %s, %s, %s, %s,
+                    %s, %s, %s, %s,
+                    %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
+                )
+            """, params)
+            resident_id = cursor.fetchone()[0]
+
+        
+        resident = Resident.objects.get(resident_id=resident_id)
+        return resident
+
+
+class ReligionCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReligionCategory
+        fields = ['religion_cat_id', 'religion_name']
+
+
 
 class ResidentSerializer(serializers.ModelSerializer):
+
+    religion = ReligionSerializer(read_only=True)
+    address = AddressSerializer(read_only=True)
+    civil_status = CivilStatusSerializer(read_only=True)
+    educational_attainment = EducationalAttainmentSerializer(read_only=True)
+    status = ResidentStatusSerializer(read_only=True)
+    
     class Meta:
         model = Resident
         fields = '__all__'
--- resident_profiling_module/models.py
+++ resident_profiling_module/models.py
@@ -8,79 +8,341 @@
 from django.db import models
 
 
-class Resident(models.Model):
-    resident_id = models.AutoField(primary_key=True)
-    last_name = models.CharField(max_length=100)
-    first_name = models.CharField(max_length=100)
-    middle_name = models.CharField(max_length=100, blank=True, null=True)
-    dob = models.DateField()
-    sex = models.CharField(max_length=10)
-    date_recorded = models.DateField()
-    religion = models.ForeignKey('Religion', models.DO_NOTHING)
-    civil_stat = models.ForeignKey('CivilStatus', models.DO_NOTHING)
-    educational_attain = models.ForeignKey('EducationalAttainment', models.DO_NOTHING)
-    is_voter = models.BooleanField()
-    # personnel = models.ForeignKey('Personnel', models.DO_NOTHING, blank=True, null=True)
-    address = models.ForeignKey('Address', models.DO_NOTHING)
-    status = models.ForeignKey('ResidentStatus', models.DO_NOTHING)
-    quarter = models.ForeignKey('Quarter', models.DO_NOTHING)
+class ActivityLog(models.Model):
+    log_id = models.AutoField(primary_key=True)
+    action = models.CharField(max_length=20)
+    subsystem = models.ForeignKey('Subsystem', models.DO_NOTHING)
+    table_name = models.CharField(max_length=100)
+    action_details = models.TextField()
+    performed_by = models.IntegerField(blank=True, null=True)
+    log_timestamp = models.DateTimeField(blank=True, null=True)
 
     class Meta:
-        db_table = 'resident'
         managed = False
+        db_table = 'activity_log'
 
-class Religion(models.Model):
-    religion_id = models.AutoField(primary_key=True)
-    religion_cat_id = models.IntegerField()  # or ForeignKey if available
-    other_religion = models.CharField(max_length=100, blank=True, null=True)
 
-    class Meta: 
-        db_table = 'religion'
+class Address(models.Model):
+    address_id = models.AutoField(primary_key=True)
+    house_number = models.CharField(max_length=50, blank=True, null=True)
+    street = models.CharField(max_length=200, blank=True, null=True)
+    barangay = models.CharField(max_length=200, blank=True, null=True)
+    sitio = models.ForeignKey('Sitio', models.DO_NOTHING, blank=True, null=True)
+    city_municipality = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+
+    class Meta:
         managed = False
+        db_table = 'address'
+
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
 
 class CivilStatus(models.Model):
     civil_stat_id = models.AutoField(primary_key=True)
-    civil_code = models.CharField(max_length=100)
-    civil_name = models.CharField(max_length=100)
+    civil_code = models.CharField(max_length=10)
+    civil_name = models.CharField(max_length=20)
 
     class Meta:
-        db_table = 'civil_status'
         managed = False
+        db_table = 'civil_status'
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.SmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
 
 
 class EducationalAttainment(models.Model):
     educational_attain_id = models.AutoField(primary_key=True)
-    educational_level = models.CharField(max_length=100)
+    educational_level = models.CharField(max_length=30)
     educational_attain_name = models.CharField(max_length=100)
 
     class Meta:
+        managed = False
         db_table = 'educational_attainment'
-        managed = False
 
-class Address(models.Model):
-    address_id = models.AutoField(primary_key=True)
-    barangay = models.CharField(max_length=100)
-    street = models.CharField(max_length=100, blank=True, null=True)
-    house_number = models.CharField(max_length=100, blank=True, null=True)
-    sitio_id = models.IntegerField(blank=True, null=True)  # or ForeignKey if available
-
-    class Meta:
-        db_table = 'address'
-        managed = False
-
-class ResidentStatus(models.Model):
-    status_id = models.AutoField(primary_key=True)
-    status_name = models.CharField(max_length=100)
-    description = models.CharField(max_length=100)
-
-    class Meta:
-        db_table = 'resident_status'
-        managed = False
 
 class Quarter(models.Model):
     quarter_id = models.AutoField(primary_key=True)
-    quarter = models.CharField(max_length=100)
+    quarter_number = models.IntegerField()
+    quarter_name = models.CharField(max_length=20)
+    year = models.IntegerField()
+    start_date = models.DateField()
+    end_date = models.DateField()
 
     class Meta:
-        db_table = 'quarter'
         managed = False
+        db_table = 'quarter'
+
+
+class Religion(models.Model):
+    religion_id = models.AutoField(primary_key=True)
+    religion_cat = models.ForeignKey('ReligionCategory', models.DO_NOTHING)
+    other_religion = models.CharField(max_length=50, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'religion'
+
+
+class ReligionCategory(models.Model):
+    religion_cat_id = models.AutoField(primary_key=True)
+    religion_code = models.CharField(unique=True, max_length=10)
+    religion_name = models.CharField(max_length=50)
+
+    class Meta:
+        managed = False
+        db_table = 'religion_category'
+
+
+class Resident(models.Model):
+    resident_id = models.AutoField(primary_key=True)
+    resident_code = models.CharField(max_length=20, unique=True, blank=True, null=True)
+    last_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50, blank=True, null=True)
+    suffix = models.CharField(max_length=10, blank=True, null=True)
+    dob = models.DateField()
+    sex = models.CharField(max_length=10)
+    gender = models.CharField(max_length=15, blank=True, null=True)
+    is_voter = models.BooleanField(blank=True, null=True)
+    email = models.CharField(max_length=100, blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    date_recorded = models.DateField()
+    religion = models.ForeignKey('Religion', on_delete=models.SET_NULL, blank=True, null=True, db_column='religion_id')
+    civil_status = models.ForeignKey('CivilStatus', on_delete=models.SET_NULL, blank=True, null=True, db_column='civil_stat_id')
+    educational_attainment = models.ForeignKey('EducationalAttainment', on_delete=models.SET_NULL, blank=True, null=True, db_column='educational_attain_id')
+    status = models.ForeignKey('ResidentStatus', on_delete=models.PROTECT, db_column='status_id')
+    address = models.ForeignKey('Address', on_delete=models.SET_NULL, blank=True, null=True, db_column='address_id')
+
+    def __str__(self):
+        return f"{self.last_name}, {self.first_name}"
+
+    class Meta:
+        managed = False
+        db_table = 'resident'
+
+
+class ResidentBirthCertificate(models.Model):
+    birth_cert_id = models.AutoField(primary_key=True)
+    resident = models.ForeignKey(Resident, models.DO_NOTHING)
+    image_data = models.BinaryField()
+    date_uploaded = models.DateTimeField(blank=True, null=True)
+    uploaded_by = models.IntegerField(blank=True, null=True)
+    verified = models.BooleanField(blank=True, null=True)
+    verification_date = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'resident_birth_certificate'
+
+
+class ResidentCredentials(models.Model):
+    account_id = models.AutoField(primary_key=True)
+    resident = models.OneToOneField(Resident, models.DO_NOTHING)
+    username = models.CharField(unique=True, max_length=50)
+    password = models.CharField(max_length=255)
+    is_active = models.BooleanField(blank=True, null=True)
+    req_pass_change = models.BooleanField(blank=True, null=True)
+    password_reset_requested_at = models.DateTimeField(blank=True, null=True)
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'resident_credentials'
+
+
+class ResidentIdDocument(models.Model):
+    id_doc_id = models.AutoField(primary_key=True)
+    resident = models.ForeignKey(Resident, models.DO_NOTHING)
+    document_type = models.CharField(max_length=50)
+    document_number = models.CharField(max_length=50, blank=True, null=True)
+    image_data = models.BinaryField()
+    date_uploaded = models.DateTimeField(blank=True, null=True)
+    uploaded_by = models.IntegerField(blank=True, null=True)
+    verified = models.BooleanField(blank=True, null=True)
+    verification_date = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'resident_id_document'
+
+
+class ResidentQuarterly(models.Model):
+    rq_id = models.AutoField(primary_key=True)
+    resident = models.ForeignKey(Resident, models.DO_NOTHING)
+    quarter = models.ForeignKey(Quarter, models.DO_NOTHING)
+    last_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50, blank=True, null=True)
+    suffix = models.CharField(max_length=10, blank=True, null=True)
+    dob = models.DateField()
+    sex = models.CharField(max_length=10)
+    gender = models.CharField(max_length=15, blank=True, null=True)
+    is_voter = models.BooleanField()
+    email = models.CharField(max_length=100, blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    date_snapshot = models.DateField(blank=True, null=True)
+    religion = models.ForeignKey(Religion, models.DO_NOTHING, blank=True, null=True)
+    civil_stat = models.ForeignKey(CivilStatus, models.DO_NOTHING, blank=True, null=True)
+    educational_attain = models.ForeignKey(EducationalAttainment, models.DO_NOTHING, blank=True, null=True)
+    status = models.ForeignKey('ResidentStatus', models.DO_NOTHING)
+    address = models.ForeignKey(Address, models.DO_NOTHING, blank=True, null=True)
+    encoded_by = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'resident_quarterly'
+
+
+class ResidentStatus(models.Model):
+    status_id = models.AutoField(primary_key=True)
+    status_name = models.CharField(max_length=20)
+    description = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'resident_status'
+
+
+class ResidentUpdateLogs(models.Model):
+    request_id = models.AutoField(primary_key=True)
+    resident = models.ForeignKey(Resident, models.DO_NOTHING)
+    request_by = models.IntegerField()
+    field_name = models.TextField()
+    current_value = models.TextField()
+    proposed_value = models.TextField()
+    date_requested = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'resident_update_logs'
+
+
+class Sitio(models.Model):
+    sitio_id = models.AutoField(primary_key=True)
+    district_number = models.IntegerField()
+    sitio_name = models.CharField(max_length=50)
+
+    class Meta:
+        managed = False
+        db_table = 'sitio'
+
+
+class Subsystem(models.Model):
+    subsystem_id = models.AutoField(primary_key=True)
+    subsystem_name = models.CharField(unique=True, max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'subsystem'
