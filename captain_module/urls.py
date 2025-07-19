ceddf83a6e78332bb857829356cc0a9609f997e0
presciousmae <presc@LAPTOP-8QNVTMUJ>
2025-07-19 15:16:26 +0800

    Added new login page and updated UI layout for household and business forms

--- captain_module/urls.py
+++ captain_module/urls.py
@@ -3,6 +3,12 @@ from . import views
 
 urlpatterns = [
      path('', views.captain_dashboard),
-     path('admin_Addpersonnel/', views.admin_Addpersonnel),
+     path('captain_viewResident', views.captain_viewResident),
+     path('captain_viewMoreResident/', views.captain_viewMoreResident),
+     path('captain_householdList/', views.captain_householdList),
+     path('captain_moreHousehold/', views.captain_moreHousehold),
+     path('captain_businessList/', views.captain_businessList),
+     path('captain_moreBusinessInfo/', views.captain_moreBusinessInfo),
+
     
 ]
