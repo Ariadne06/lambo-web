af32758d2c42b8a281bb00a57a1d81573a5b3b8c
CharlzAce <charlzdereck.arranchado@ctu.edu.ph>
2025-08-06 16:39:12 +0800

    Update Login

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+from django.db import models, connection
 
-# Create your models here.
+class logging(models.Model):
+    
+    @staticmethod
+    def sp_login_personnel_web(username, password):
+        try:
+            with connection.cursor() as cursor:
+                cursor.callproc(
+                    'login_personnel_web', [
+                        username, password
+                    ]
+                )
+                result = cursor.fetchone()
+                return result[0]
+        except Exception as e:
+            raise e
