5806bafa63f619176d0d1fed92a2984fbcfea0f9
CharlzAce <charlzdereck.arranchado@ctu.edu.ph>
2025-08-06 18:47:15 +0800

    Update

--- lambo/settings.py
+++ lambo/settings.py
@@ -12,11 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-<<<<<<< HEAD
-=======
 from supabase import create_client, Client
 
->>>>>>> c0b579d819685b2498324fbdede5ea90beb485ad
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
