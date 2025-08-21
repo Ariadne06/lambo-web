70032d26c03abe139d7b9b6ab093ac827a258eec
CharlzAce <charlzdereck.arranchado@ctu.edu.ph>
2025-08-22 01:31:27 +0800

    Running Tailwind Automatically

--- authentication/urls.py
+++ authentication/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('silent_logout/', views.silent_logout, name='silent_logout'),
-    # path("tab-close-logout/", views.tab_close_logout, name="tab_close_logout"),
+    path('req_pwd_change/', views.req_pwd_change, name='req_pwd_change'),
 ]
