319bb39a20684e3888e416a4477336a494e7e9c7
CharlzAce <charlzdereck.arranchado@ctu.edu.ph>
2025-09-03 14:30:35 +0800

    Activity Logs Fixed

--- captain_module/views.py
+++ captain_module/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from authentication.decorators import custom_login_required, role_required
+from utils.flash import set_flash, get_flash
+from utils.db_message import _clean_db_error
+from .models import Captain
 
 # Create your views here.
 
@@ -39,6 +42,40 @@ def captain_moreBusinessInfo(request):
     return render(request, 'captain_module/captain_moreBusinessInfo.html')
 
 @custom_login_required
-@role_required('Captain')
+@role_required('Barangay Captain')
 def personnelRequest(request):
-    return render(request, 'captain_module/personnelRequest.html')
+    flash = get_flash(request) 
+    
+    if request.method == 'POST':
+        pid = int(request.POST.get('pid'))
+        new_status = request.POST.get('action')
+
+        try:
+            captain_personnel_id = request.session.get('personnel_id')
+            
+            result = Captain.sp_review_personnel_account_by_captain(pid, new_status, captain_personnel_id)
+            
+            if result is None:
+                raise Exception("Failed to change approval status.")
+            
+            msg = result[0]
+            set_flash(request, msg, "success")
+        except Exception as e:
+            msg = _clean_db_error(e)
+            set_flash(request, msg, "error")
+
+        return redirect('captain_module:personnelRequest')
+
+    # # GET: live search + filter
+    # query = (request.GET.get('query') or '').strip()
+    # filter_status = (request.GET.get('filter_status') or '').strip()
+
+    results = Captain.sp_get_pending_personnel_requests(None)
+
+    return render(request, 'captain_module/personnelRequest.html', {
+        'results': results,
+        # 'query': query,
+        # 'filter_status': filter_status,
+        'message': flash['message'],
+        'message_level': flash['message_level'],
+    })
