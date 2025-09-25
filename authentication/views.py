7d4dd1abeb44b4c90225199a1305de93cea84df7
CharlzAce <charlzdereck.arranchado@ctu.edu.ph>
2025-09-26 00:40:08 +0800

    Changing Error Message for mobile fogot pass

--- authentication/views.py
+++ authentication/views.py
@@ -276,7 +276,7 @@ def api_forgot_password(request):
     # 2) Verify this pair exists in your DB (use your stored proc)
     try:
         res = logging.sp_request_password_reset(username, email)
-        if res is not True:
+        if not res:
             return JsonResponse({"error": "User not found for that username+email"}, status=404)
     except Exception as e:
         return JsonResponse({"error": _clean_db_error(e)}, status=500)
