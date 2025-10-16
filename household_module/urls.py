0270dd2c6f2d9eea460c54eddcd435681406f660
CharlzAce <charlzdereck.arranchado@ctu.edu.ph>
2025-10-16 23:36:37 +0800

    Fixing Default urls.py of household_module

--- household_module/urls.py
+++ household_module/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
+import django.contrib.admin as admin
 
 app_name = 'household_module'
 
 urlpatterns = [
     # Include router URLs
-    path('', include()),
+    path('admin/', admin.site.urls),
 ]
