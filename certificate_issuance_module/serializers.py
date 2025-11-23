41d709093750b156edf849c4f6e053441e5d5cb0
Ariadne06 <adrianne.villarta@ctu.edu.ph>
2025-11-23 17:09:06 +0800

    add create_clearance_application and revision of purposesviewset

--- certificate_issuance_module/serializers.py
+++ certificate_issuance_module/serializers.py
@@ -13,4 +13,16 @@ class ClearancePurposeSerializer(serializers.ModelSerializer):
     """Serializer for clearance purposes."""
     class Meta:
         model = ClearancePurpose
-        fields = ['clearance_purpose_id', 'purpose_name', 'fee']
+        fields = ['other_clearance_id', 'purpose_name', 'fee_amount']
+
+
+class CreateBarangayClearanceSerializer(serializers.Serializer):
+    """Serializer for creating a barangay clearance application."""
+    resident_id = serializers.IntegerField(required=True, help_text="ID of the resident requesting clearance")
+    other_clearance_id = serializers.IntegerField(required=True, help_text="ID of the clearance purpose type")
+
+
+class BarangayClearanceResponseSerializer(serializers.Serializer):
+    """Serializer for barangay clearance creation response."""
+    application_id = serializers.IntegerField(help_text="ID of the created clearance application")
+    message = serializers.CharField(help_text="Success message")
