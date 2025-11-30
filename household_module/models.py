efe54828c457873e888afc6f8c0f7aead71f8e67
presciousmae <presc@LAPTOP-8QNVTMUJ>
2025-11-30 20:22:44 +0800

    nurse

--- household_module/models.py
+++ household_module/models.py
@@ -289,7 +289,7 @@ class ChildImmunizationRecord(models.Model):
     date_added = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        managed = False
+        managed = False  # uses existing DB table
         db_table = 'child_immunization_record'
 
 
